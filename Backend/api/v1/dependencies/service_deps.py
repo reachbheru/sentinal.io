022e938cf0a9540bf6cfea51ab520e5e1d230efb
reach bheru <reachbheru@gmail.com>
2025-09-03 17:57:48 +0530

    service dependency and query route

--- Backend/api/v1/dependencies/service_deps.py
+++ Backend/api/v1/dependencies/service_deps.py
@@ -1,4 +1,10 @@
 from Backend.core.v1.llm.llm_client import LLMClient
+from Backend.services.v1.query import QueryService
+from Backend.api.v1.schemas.query.request import UserQuery
 
-async def get_gemini():
-    return LLMClient().gemini()
+def get_gemini():
+    return  LLMClient().gemini()
+
+def get_query_service(user_query: UserQuery):
+    llm = get_gemini()
+    return QueryService(user_query=user_query, llm=llm)
