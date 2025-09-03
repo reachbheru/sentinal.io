0f00bb7778a5cffec38d6f1bcda81fe8526df6af
reach bheru <reachbheru@gmail.com>
2025-09-03 17:57:29 +0530

    constants

--- Backend/config/v1/constants/general.py
+++ Backend/config/v1/constants/general.py
@@ -4,3 +4,5 @@ from pathlib import Path
 # Path to the cellbot package folder
 PACKAGE_ROOT = Path(__file__).parent.parent.parent.parent
 PROMPTS_PATH = PACKAGE_ROOT / "prompts/v1/"  # Path to the prompt templates directory
+
+enhancement_suffix = 'latest news interview scandal controversy achievements public appearance social media video 2025'
